44a33d2da5c28ccd88c94af488834e319b4916dd
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-07-12 14:15:12 +0200

    Established a database connection using an instance of mysqli class

--- signin.php
+++ signin.php
@@ -2,14 +2,18 @@
 
     require_once "connect.php";
 
-    $connect = new mysqli($host, $db_user, $db_password, $db_name);
+    $connect = new mysqli($host, $db_user, $db_passowrd, $db_name);
 
    if($connect->connect_errno!=0)
    {
-    echo "Error:".$connect->connect.errno ."Opis: ".$connect->connect_error;
+    echo "Error:".$connect->connect_errno."Opis:".$connect->connect_error;
    } 
-
+   else
+   {
     $login = $_POST['login'];
     $password = $_POST['password'];
+    echo "It works!";
+    $connect->close();
+   }
 
 ?>
